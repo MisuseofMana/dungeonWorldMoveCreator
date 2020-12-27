f05093b965c5aed652c61c81ca5ba617ae25c745
Sean Yager <contact@seanyager.com>
2020-12-26 19:14:10 -0600

    created full success component

---
+++ src/components/moveMaking/FullSuccess.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <p>Define a full success.</p>
+      <input value="fullSuccess" v-model="fullSuccess">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'FullSuccess',
+        computed: {
+        fullSuccess:{
+          get() {
+              return this.$store.state.fullSuccess
+          },
+          set (value) {
+              this.$store.commit('mutate', {property:'trigger', with:value})
+          }
+      },
+    }
+    }
+</script>
+
+<style scoped>
+
+</style>
