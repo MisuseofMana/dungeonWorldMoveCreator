561b7cc169d03f1fa64931fdd73bf8d5769de308
Sean Yager <contact@seanyager.com>
2020-12-26 19:17:38 -0600

    created navigation bar

---
+++ src/components/navigation/NavigationBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>
+          <p v-if="currentPage === 'start'" class="siteButton" @click="$emit('change-page', 'next')">GET STARTED</p>
+        </div>
+
+        <div v-if="currentPage !== 'start'">
+          <div class="siteButton" @click="$emit('change-page', 'back')">BACK</div>
+          <div class="siteButton" @click="$emit('change-page', 'next')">NEXT</div>
+        </div>
+        
+        <div v-if="currentPage === 'review'">
+          <div class="siteButton">FINISHED</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'NaviagtionBar',
+    props:['currentPage'],
+
+    }
+</script>
+
+<style scoped>
+
+</style>
