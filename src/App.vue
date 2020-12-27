310d8e0e485a300eab2c59cf6108e95393c4de4f
Sean Yager <contact@seanyager.com>
2020-12-26 19:11:34 -0600

    refactored app file into more modules

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,79 @@
 <template>
   <div id="app">
-    <MainScreen msg="Welcome to Your Vue.js App"/>
+    <InstructionsScreen :currentPage="step"/>
+    
+    <transition name="fade" mode="out-in">
+
+      <MoveTrigger v-if="step === 'moves'"/>
+      <RelevantStat v-if="step === 'stats'"/>
+      <FullSuccess v-if="step === 'full'"/>
+      <MixedSuccess v-if="step === 'mixed'"/>
+      <AdditionalInfo v-if="step === 'additional'" />
+      <FinalReview v-if="step === 'review'" />
+    </transition>
+
+    <TheMove :currentPage="step" />
+
+    <NavigationBar @change-page="changeStep($event)" :currentPage="step"/>
   </div>
 </template>
 
 <script>
-import MainScreen from './components/MainScreen.vue'
-import './styles/globals.css'
+import InstructionsScreen from './components/InstructionsScreen.vue';
+import NavigationBar from './components/navigation/NavigationBar.vue';
+
+import MoveTrigger from './components/moveMaking/MoveTrigger.vue';
+import RelevantStat from './components/moveMaking/RelevantStat.vue';
+import FullSuccess from './components/moveMaking/FullSuccess.vue';
+import MixedSuccess from './components/moveMaking/MixedSuccess.vue';
+import AdditionalInfo from './components/moveMaking/AdditionalInfo.vue';
+import FinalReview from './components/moveMaking/FinalReview.vue';
+
+import TheMove from './components/moveMaking/TheMove.vue';
+
+import './styles/globals.css';
 
 export default {
   name: 'App',
   components: {
-    MainScreen
+    InstructionsScreen,
+    NavigationBar,
+    MoveTrigger,
+    RelevantStat,
+    FullSuccess,
+    MixedSuccess,
+    AdditionalInfo,
+    FinalReview,
+    TheMove
+  },
+  data() {
+    return {
+      step: 'start',
+      progression: [
+        'start',
+        'moves',
+        'stats',
+        'full',
+        'mixed',
+        'additional',
+        'review'
+      ]
+    }
+  },
+  methods: {
+    changeStep(event){
+        if(event === 'next' && this.step !== 'review') {
+          const currentStepIndex = this.progression.indexOf(this.step);
+          this.step = this.progression[currentStepIndex + 1];
+        }
+        if(event === 'back' && this.step !== 'start') {
+          const currentStepIndex = this.progression.indexOf(this.step);
+          this.step = this.progression[currentStepIndex - 1];
+        }
+        if(event === 'finished') {
+          return
+        }
+    }
   }
 }
 </script>
