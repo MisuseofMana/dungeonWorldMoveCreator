202aaf966da8aa655ef49c9db35dc6fb8dd9c280
Sean Yager <contact@seanyager.com>
2020-12-26 19:17:04 -0600

    created component

---
+++ src/components/moveMaking/MoveTrigger.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="flexRow">
+    <div class="module">
+      <textarea style="resize: none;" value="trigger" v-model="trigger"></textarea>
+    </div>
+
+    <div class="module">
+      <div class="flexRow">
+        <div class="radios">
+          <label for="+">+</label>
+          <input checked type="radio" class="radioButton" value="+" v-model="plusminus">
+        </div>
+
+        <span class="radios">
+          <label for="-">-</label>
+          <input type="radio" class="radioButton" value="-" v-model="plusminus">
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoveTrigger',
+  computed:{
+    trigger:{
+        get() {
+            return this.$store.state.trigger
+        },
+        set (value) {
+            this.$store.commit('mutate', {property:'trigger', with:value})
+        }
+    },
+    plusminus:{
+        get() {
+            return this.$store.state.plusminus
+        },
+        set (value) {
+            this.$store.commit('mutate', {property:'plusminus', with:value})
+        }
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .module {
+    margin:20px;
+  }
+  .radios {
+    font-size:20px;
+    font-weight:600;
+  }
+  
+  .radioButton {
+    margin:10px;
+  }
+
+  textarea {
+    background:rgb(211, 211, 211);
+    width:200px;
+    height:40px;
+    padding:10px;
+    border: none;
+    overflow: auto;
+    outline: none;
+
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+
+    resize: none; /*remove the resize handle on the bottom right*/
+  }
+</style>
---
+++ src/components/moveMaking/RelevantStat.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="flexCol">
+      <select v-model="stat" v-if="!isCustom">
+        <option disable value="">---</option>
+        <option>STR</option>
+        <option>DEX</option>
+        <option>CON</option>
+        <option>INT</option>
+        <option>WIS</option>
+        <option>CHA</option>
+      </select>
+
+      <span v-if="isCustom">
+        <input value="CUSTOM" v-model="stat">
+      </span>
+
+      <span class="modifier">
+			<span class="siteButton" 
+			v-if="isCustom"
+			@click="isCustom = !isCustom">
+			PICK MOD FROM STATS
+			</span>
+
+			<span class="siteButton" 
+			v-if="!isCustom"
+			@click="isCustom = !isCustom">
+			MAKE CUSTOM MOD
+			</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'RelevantStat',
+        data() {
+          return {
+            isCustom: false
+          }
+        },
+        computed:{
+            stat:{
+            get() {
+                return this.$store.state.stat
+            },
+            set (value) {
+                this.$store.commit('mutate', {property:'stat', with:value})
+            }
+          },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
